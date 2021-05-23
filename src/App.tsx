9f29095a845724c668bd2c74c6de3d9d3e23f03d
Daniil <a03zcode@yandex.ru>
2021-05-23 20:44:43 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,29 @@ import '../node_modules/reseter.css/css/reseter.min.css'
 import Form from './form/Form'
 import unsplash from './API/api'
 import MappedImages from './components/MappedImages'
+import deleteIcon from './delete.svg'
 
 interface SearchI {
-    searchImage: string
+	searchImage: string
 }
 
 function App() {
-    // prettier-ignore
-    const [images, setImages] = useState<Array<{ id: number; urls: { regular: string } }>>([])
+	// prettier-ignore
+	const [images, setImages] = useState<Array<{ id: number; urls: { regular: string } }>>([])
 
-    const getPhotos = async (data: SearchI) => {
-        const res = await unsplash.get(
-            `/search/photos?query=${data.searchImage}&per_page=100`
-        )
-        setImages(res.data.results)
-    }
+	const getPhotos = async (data: SearchI) => {
+		const res = await unsplash.get(`/search/photos?query=${data.searchImage}&per_page=100`)
+		setImages(res.data.results)
+	}
 
-    return (
-        <div className="App">
-            <h1 className="mainHeading">Search image by keyword </h1>
-            <Form onSubmit={getPhotos} />
-            <MappedImages images={images} />
-        </div>
-    )
+	return (
+		<div className='App'>
+			<h1 className='mainHeading'>Search image by keyword </h1>
+			<Form onSubmit={getPhotos} />
+			<img src={deleteIcon} alt='' />
+			<MappedImages images={images} />
+		</div>
+	)
 }
 
 export default App
